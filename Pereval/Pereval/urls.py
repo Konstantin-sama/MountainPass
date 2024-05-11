a3cc649faa36f1670b051976423d47dbdb9eb78a
Konstantin <mine@gmail.com>
2024-05-12 00:38:58 +0300

    1. Проверка пользователя по id перед созданием нового объекта в файле serializers.py. 2. Новая фича yasg.py. 3. Изменение поля status после модерации. Блокирует обновление данных.

--- Pereval/Pereval/urls.py
+++ Pereval/Pereval/urls.py
@@ -19,9 +19,10 @@ from django.urls import include, path
 from rest_framework import routers
 
 from mount import views
+from .yasg import urlpatterns as doc_urls
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewset, basename='users')
+router.register(r'tourist_id', views.UserViewset, basename='tourist_id')
 router.register(r'coords', views.CoordsViewset, basename='coords')
 router.register(r'levels', views.LevelViewset, basename='levels')
 router.register(r'images', views.ImageViewset, basename='images')
@@ -32,3 +33,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+urlpatterns += doc_urls
