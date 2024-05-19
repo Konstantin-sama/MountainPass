012248742883826634e073ea0a96cc3adaffe0e7
Konstantin <mine@gmail.com>
2024-05-19 22:07:54 +0300

    Изменения файла serializers.py: отладка функции для проверки данных Users. Изменение в поле status(NW). Изменения в поле add_time и status = serializers.CharField(read_only=True).
    Signed-off-by: Konstantin <mine@gmail.com>

--- Pereval/mount/serializers.py
+++ Pereval/mount/serializers.py
@@ -49,7 +49,9 @@ class PerevalSerializer(WritableNestedModelSerializer):
         coord_id = validated_data.pop('coord_id')
         level = validated_data.pop('level')
         images = validated_data.pop('images')
+
         tourist_id, created = Users.objects.get_or_create(**tourist_id)
+
         coord_id = Coords.objects.create(**coord_id)
         level = Level.objects.create(**level)
         pereval = Pereval.objects.create(**validated_data, tourist_id=tourist_id, coord_id=coord_id, level=level)
